42fbc602249b3c7be199346de9c36ca06d2bd70e
Eugenio Kuri <eugenio.kuri@honeywell.com>
2018-08-02 17:44:12 -0500

    first commit

---
+++ components/MenuComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, FlatList } from 'react-native';
+import{ListItem} from 'react-native-elements';
+
+function Menu(props){
+    
+}
+
+export default Menu;
